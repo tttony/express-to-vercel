fbf4bcd664df0bcab69223a0533763b93fd7f9e3
Tony <219885+tttony@users.noreply.github.com>
2022-06-09 15:56:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+});
+
+app.get('/home', (req, res) => {
+    res.send('Homie!');
 })
 
 app.listen(process.env.PORT || 3000);
